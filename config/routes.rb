c69c569bfacaf33ef197c3a7be0a5e044d9f3b63
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-27 10:49:01 -0600

    adding users

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
